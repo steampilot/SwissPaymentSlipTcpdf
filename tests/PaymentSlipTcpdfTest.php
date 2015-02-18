eca6581688f1c15631319e19fad5ee58549489c2
Marc Würth <ravage@bluewin.ch>
2015-02-19 00:43:11 +0100

    Updated the file header DocBlock everywhere

--- tests/PaymentSlipTcpdfTest.php
+++ tests/PaymentSlipTcpdfTest.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Swiss Payment Slip TCPDF
+ *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright 2012-2015 Some nice Swiss guys
+ * @author Marc Würth ravage@bluewin.ch
+ * @author Manuel Reinhard <manu@sprain.ch>
+ * @author Peter Siska <pesche@gridonic.ch>
+ * @link https://github.com/ravage84/SwissPaymentSlipTcpdf/
+ */
 
 namespace SwissPaymentSlip\SwissPaymentSlipTcpdf\Tests;
 
