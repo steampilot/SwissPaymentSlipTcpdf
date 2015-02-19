8d8f30ff521a602219d355e240a5194892630e4a
Marc Würth <ravage@bluewin.ch>
2015-02-19 03:02:08 +0100

    CS

--- examples/index.php
+++ examples/index.php
@@ -20,9 +20,21 @@
 <h1>Examples for SwissPaymentSlipTcpdf</h1>
 <p>Learn how to use the SwissPaymentSlipFpdf class with the following examples:</p>
 <ul>
-	<li><a href="SwissPaymentSlipTcpdf_Orange_Slip.php">01 - Create an orange payment slip with SwissPaymentSlipTcpdf</a></li>
-	<li><a href="SwissPaymentSlipTcpdf_Red_Slip.php">02 - Create a red payment slip with SwissPaymentSlipTcpdf</a></li>
-	<li><a href="SwissPaymentSlipTcpdf_Thousand_Slips.php">03 - Create one thousand orange payment slips with SwissPaymentSlipTcpdf</a></li>
+	<li>
+        <a href="SwissPaymentSlipTcpdf_Orange_Slip.php">
+            01 - Create an orange payment slip with SwissPaymentSlipTcpdf
+        </a>
+    </li>
+	<li>
+        <a href="SwissPaymentSlipTcpdf_Red_Slip.php">
+            02 - Create a red payment slip with SwissPaymentSlipTcpdf
+        </a>
+    </li>
+	<li>
+        <a href="SwissPaymentSlipTcpdf_Thousand_Slips.php">
+            03 - Create one thousand orange payment slips with SwissPaymentSlipTcpdf
+        </a>
+    </li>
 </ul>
 </body>
 </html>
