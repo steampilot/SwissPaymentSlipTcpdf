af733574f6bdbd274c8c991841932df44d3e4566
Marc Würth <ravage@bluewin.ch>
2015-04-01 13:50:30 +0200

    CS

--- src/PaymentSlipTcpdf.php
+++ src/PaymentSlipTcpdf.php
@@ -166,13 +166,13 @@ class PaymentSlipTcpdf extends PaymentSlipPdf
         $hexStr = preg_replace("/[^0-9A-Fa-f]/", '', $hexStr); // Gets a proper hex string
         $rgbArray = array();
         if (strlen($hexStr) == 6) {
-            //If a proper hex code, convert using bitwise operation. No overhead... faster
+            // If a proper hex code, convert using bitwise operation. No overhead... faster
             $colorVal = hexdec($hexStr);
             $rgbArray['red'] = 0xFF & ($colorVal >> 0x10);
             $rgbArray['green'] = 0xFF & ($colorVal >> 0x8);
             $rgbArray['blue'] = 0xFF & $colorVal;
         } elseif (strlen($hexStr) == 3) {
-            //if shorthand notation, need some string manipulations
+            // If shorthand notation, need some string manipulations
             $rgbArray['red'] = hexdec(str_repeat(substr($hexStr, 0, 1), 2));
             $rgbArray['green'] = hexdec(str_repeat(substr($hexStr, 1, 1), 2));
             $rgbArray['blue'] = hexdec(str_repeat(substr($hexStr, 2, 1), 2));
